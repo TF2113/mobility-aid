a1baa61ae8d6924412d7614490c71107448e7be7
TF2113 <148654475+TF2113@users.noreply.github.com>
2025-06-11 21:44:24 +0100

    Fix indentation

--- src/cam/capture_image.cpp
+++ src/cam/capture_image.cpp
@@ -5,19 +5,19 @@
 int main() {
 
     for (int i = 0; i < 5; i ++){
-    const std::string imagePath = "builds/captures/image" + std::to_string(i) + ".jpg";
+        const std::string imagePath = "builds/captures/image" + std::to_string(i) + ".jpg";
 
-    // Use libcamera-still command to capture image
-    std::string cmd = "libcamera-jpeg -n -t 1 --width 640 --height 480 -o " + imagePath;
-    int returnValue = system(cmd.c_str());
+        // Use libcamera-still command to capture image
+        std::string cmd = "libcamera-jpeg -n -t 1 --width 640 --height 480 -o " + imagePath;
+        int returnValue = system(cmd.c_str());
 
-    if (returnValue != 0) {
-        std::cerr << "Error: libcamera-still failed." << std::endl;
-        return 1;
+        if (returnValue != 0) {
+            std::cerr << "Error: libcamera-still failed." << std::endl;
+            return 1;
+        }
+
+        std::cout << "Image successfully captured." << std::endl;
     }
-
-    std::cout << "Image successfully captured ." << std::endl;
-}
 
     return 0;
 }
