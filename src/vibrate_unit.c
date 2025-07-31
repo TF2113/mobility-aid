31658e70225e1c500af8b0ba5df64b2aab86ce58
TF2113 <148654475+TF2113@users.noreply.github.com>
2025-07-31 18:24:31 +0100

    Create new version of vibrate.c without main for testing logic

---
+++ src/vibrate_unit.c
@@ -0,0 +1,80 @@
+#include "vibrate.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/file.h>
+#include "gpio_functions.h"
+
+#define GPIO_OFFSET 0x0 // Storing /dev/gpiomem in virtual memory via mmap()
+#define MEM_BLOCK 4096  // 4KB memory block for storing register data
+
+#define VIB_MOTOR 13
+
+int vibrate(int count, double duration, double delay){
+
+    int fd = open("/dev/gpiomem", O_RDWR | O_SYNC); // READ & WRITE perms and SYNC to prevent program from continuing before writes are finished
+
+    if (fd < 0)
+    {
+        perror("Access to gpiomem failed\n");
+        return 1;
+    }
+
+    // 32 bit pointer to gpiomem for manipulating GPIO register via mmap()
+    volatile uint32_t *gpio = mmap(NULL,                   // Kernel chooses virtual memory address
+                                   MEM_BLOCK,              // Size of data to be mapped
+                                   PROT_READ | PROT_WRITE, // Allows data to be read and written to
+                                   MAP_SHARED,             // Other processes can use data
+                                   fd,                     // File descriptor (gpiomem)
+                                   GPIO_OFFSET);           // Start of GPIO address within gpiomem (0x0)
+
+    if (gpio == MAP_FAILED){
+        perror("Failed to map gpio registers\n");
+        close(fd);
+        return 1;
+    }
+
+    int lock_fd = open("./builds/tmp/vibrate.lock", O_CREAT | O_RDWR, 0666);
+    if (lock_fd < 0) {
+        perror("open lock file");
+        return 1;
+    }
+    if (flock(lock_fd, LOCK_EX | LOCK_NB) < 0) {
+        close(lock_fd);
+        return 0;
+    }
+
+    gpioSetFunction(gpio, VIB_MOTOR, 0b001);
+
+    for (int i = 0; i < count; i++){
+        gpioSet0(gpio, VIB_MOTOR);
+        usleep((int)(duration * 1000000));
+        gpioClear0(gpio, VIB_MOTOR);
+        usleep((int)(delay * 1000000));
+    }
+
+    munmap((void *)gpio, MEM_BLOCK); // Unmap memory
+    close(fd);                       // Close /dev/gpiomem file
+    flock(lock_fd, LOCK_UN);
+    close(lock_fd);
+    return 0;
+}
+
+int cli_vibrate(int argc, char *argv[]){
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <vibration_count> <duration(s)> <delay(s)>\n", argv[0]);
+        return 1;
+    }
+
+    int count = atoi(argv[1]);
+    double duration = atof(argv[2]);
+    double delay = atof(argv[3]);
+    if (count <= 0 || duration < 0 || delay < 0) {
+        fprintf(stderr, "Error: count, duration and delay must be positive.\n");
+        return 1;
+    }
+
+    return vibrate(count, duration, delay);
+}
