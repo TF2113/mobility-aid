79b386fd3edbe193a1fb49bd3815aceb2c24ff5f
TF2113 <148654475+TF2113@users.noreply.github.com>
2025-08-05 14:23:04 +0100

    Add tests for various values being passed to the SQL statement

--- test/test_db_config.c
+++ test/test_db_config.c
@@ -45,6 +45,14 @@ void test_get_int_config_key_not_found(void) {
     TEST_ASSERT_EQUAL_INT(150, result);
 }
 
+void test_get_int_config_table_not_found(void) {
+    sqlite3 *test_db;
+    sqlite3_open(":memory:", &test_db);
+    int result = get_int_config(test_db, "key", 500);
+    TEST_ASSERT_EQUAL_INT(500, result);
+    sqlite3_close(test_db);
+}
+
 void test_get_int_config_invalid_value(void) {
     insert_config("prox_yellow_led", "number");
     int result = get_int_config(db, "prox_yellow_led", 99);
@@ -55,4 +63,23 @@ void test_get_int_config_negative_value(void) {
     insert_config("neg_val", "-50");
     int result = get_int_config(db, "neg_val", 99);
     TEST_ASSERT_EQUAL_INT(-50, result);
+}
+
+void test_get_int_config_null_value(void) {
+    char *err_msg = 0;
+    sqlite3_exec(db, "INSERT INTO config (key, value) VALUES ('null_key', NULL);", 0, 0, &err_msg);
+    int result = get_int_config(db, "null_key", 250);
+    TEST_ASSERT_EQUAL_INT(250, result);
+}
+
+void test_get_int_config_empty_value(void) {
+    insert_config("empty_key", "");
+    int result = get_int_config(db, "empty_key", 99);
+    TEST_ASSERT_EQUAL_INT(0, result);
+}
+
+void test_get_int_config_valid_zero_value(void) {
+    insert_config("zero_key", "0");
+    int result = get_int_config(db, "zero_key", 99);
+    TEST_ASSERT_EQUAL_INT(0, result);
 }
--- test/test_db_runner.c
+++ test/test_db_runner.c
@@ -5,8 +5,12 @@
 
 void test_get_int_config_key_found(void);
 void test_get_int_config_key_not_found(void);
+void test_get_int_config_table_not_found(void);
 void test_get_int_config_invalid_value(void);
 void test_get_int_config_negative_value(void);
+void test_get_int_config_null_value(void);
+void test_get_int_config_empty_value(void);
+void test_get_int_config_valid_zero_value(void);
 
 int main(void) {
 
@@ -14,8 +18,12 @@ int main(void) {
 
     RUN_TEST(test_get_int_config_key_found);
     RUN_TEST(test_get_int_config_key_not_found);
+    RUN_TEST(test_get_int_config_table_not_found);
     RUN_TEST(test_get_int_config_invalid_value);
     RUN_TEST(test_get_int_config_negative_value);
-
+    RUN_TEST(test_get_int_config_null_value);
+    RUN_TEST(test_get_int_config_empty_value);
+    RUN_TEST(test_get_int_config_valid_zero_value);
+    
     return UNITY_END();
 }
